9a0d7a64a500b6755d914e1ff986768483616b87
Allan Lin <allanlin97@gmail.com>
2022-05-03 15:44:40 -0400

    creating scheduling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root "schedules#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/schedules", to: "schedules#index"
 end
